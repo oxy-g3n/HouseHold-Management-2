c6ebbe3bb5cb6d8554312cdba58ded0dbf6dbab2
oxy-g3n <unstopabledestroyer77@gmail.com>
2024-12-02 22:54:39 +0530

    updated the backend

--- backend/flaskProject/app.py
+++ backend/flaskProject/app.py
@@ -1,17 +1,10 @@
-from functools import wraps
-
-from flask import Flask, render_template, request, redirect, url_for, jsonify, flash, Response, send_file
-import jwt
-import sqlite3
-import bcrypt
-import datetime
-from api import create_app
-
+from flask import Flask, render_template
+from project_APIs import create_app
 from flask_cors import CORS
 from itsdangerous import URLSafeTimedSerializer
 app = Flask(__name__)
-SECRET_KEY = 'SUPER_SECRET_KEY'
-app.secret_key = SECRET_KEY #Required for flashing messages
+SECRET_KEY = 'A_KEY'
+app.secret_key = SECRET_KEY
 app.config['SECRET_KEY'] = SECRET_KEY
 CORS(app, resources={r"/*": {"origins": "*"}})
 s = URLSafeTimedSerializer(SECRET_KEY)
@@ -19,7 +12,7 @@ s = URLSafeTimedSerializer(SECRET_KEY)
 @app.route('/')
 @app.route('/home')
 def index():
-	return render_template('index.html')
+	return "Hello World!"
 
 
 app = create_app()
